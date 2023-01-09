d84422ef8eb48dd2e66b298b0ae16b1054021a00
MatteoCarbone17 <matteocarbone17@live.com>
2023-01-09 14:44:17 +0100

    install npm, create scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,31 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import HeaderDcComics from './components/HeaderDcComics.vue'
+import MainDcComics from './components/MainDcComics.vue'
+import FooterDcComics from './components/FooterDcComics.vue'
+
+export default {
+
+  components:{
+     HeaderDcComics,
+     MainDcComics,
+     FooterDcComics,
+
+  }
+ 
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+ <FooterDcComics />
+ <HeaderDcComics />
+ <MainDcComics />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+@use './style/general.scss' as *;
+@use '../node_modules/bootstrap/scss/bootstrap.scss' as *;
+
 </style>
+
---
+++ src/components/HeaderDcComics.vue
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<template>
+ <section class="container-fluid">
+    <div class="row">
+        <div class="col-6">
+            <img src="../assets/img/dc-logo.png" alt="" srcset="">
+        </div>
+        <div class="col-6"></div>
+    </div>
+ </section>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
