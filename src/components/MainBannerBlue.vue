d46d83f9b6b3454cc0e964f96507b0846e6dcf26
MatteoCarbone17 <matteocarbone17@live.com>
2023-01-10 18:26:54 +0100

    bonus banner completed

--- src/components/MainBannerBlue.vue
+++ src/components/MainBannerBlue.vue
@@ -1,15 +1,48 @@
 
 <script>
 export default {
-    
+data() {
+    return {
+        bannerBlueItem :[
+            {
+                image: 'buy-comics-digital-comics.png',
+                text: 'digital comics'
+            },
+            {
+                image: 'buy-comics-merchandise.png',
+                text: 'dc merchandise'
+            },
+            {
+                image: 'buy-comics-shop-locator.png',
+                text: 'comic shop locator'
+            },
+            {
+                image: 'buy-comics-subscriptions.png',
+                text: 'subscriptions'
+            },
+            {
+                image: 'buy-dc-power-visa.svg',
+                text: 'comic shop locator'
+            },
+        ]
+
+  }
+},
+methods: {
+    getImagePath: function(imgPath) {
+        return new URL(`../assets/img/${imgPath}`, import.meta.url).href;
+    }
+}    
 }
 </script>
 <template>
-    <div class="div-banner d-flex justify-content-center  align-items-center"> 
-        <img src="" alt="">
-        <p>
-          dasdasdasdasdasd
-        </p>
+    <div  class="div-banner d-flex justify-content-center align-items-center" >
+        <div class="d-flex align-items-center" v-for="item in  bannerBlueItem" > 
+            <img :src="getImagePath(item.image ) " :alt=" item.text  ">
+            <p class="text-light ms-2">
+                {{ item.text  }}
+            </p>
+        </div>
     </div>
 </template>
 
@@ -18,6 +51,11 @@ export default {
 <style lang="scss" scoped >
     .div-banner{
         background-color: rgb(2, 130, 249);
-        height: 300px;
+        height: 270px;
+    }
+
+    img{
+        height: 60px;
+        margin-left: 3rem;
     }
 </style>
